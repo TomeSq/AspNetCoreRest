404b9d85ce1e269ec56d5b10171489125cfdbfca
kenichi_shintome <kshintome@say-tech.co.jp>
2019-07-25 16:17:43 +0900

    POSTメソッドの実装

--- TodoApi/Controllers/v1/TodoController.cs
+++ TodoApi/Controllers/v1/TodoController.cs
@@ -45,6 +45,16 @@ namespace TodoApi.Controllers.v1
             return todoItem;
         }
 
-        
+        // POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+        }
+
+
     }
 }
